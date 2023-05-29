7c78b4a097eebda27a30d6bf18285a2310303959
SantiagoC16 <6317@holbertonstudents.com>
2023-05-29 08:37:41 -0700

    task 7

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+word = "Holberton"
+wordone = f"{word}"
+wordtwo = f"{word}"
+wordtree = f"{word}"
+print(f"First 3 letters: {wordone[:3]}")
+print(f"Last 2 letters: {wordtwo[-2:]}")
+print(f"Middle word: {wordtree[1:8]}")

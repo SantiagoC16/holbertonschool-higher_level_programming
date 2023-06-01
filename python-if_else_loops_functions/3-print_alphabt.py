eab400d601f038fa24485fa6607d53d5d585e0d5
SantiagoC16 <6317@holbertonstudents.com>
2023-06-01 09:33:47 -0700

    task 3 indentation

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "{}"
 for alpha in range (97, 123):
-    if alpha != 101 and alpha != 113:
-        print(str.format(chr(alpha)), end="")
+	if alpha != 101 and alpha != 113:
+		print(str.format(chr(alpha)), end="")

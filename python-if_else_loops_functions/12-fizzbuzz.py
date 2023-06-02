cc0cc85654eadc985fa28a0b04b6564db3c880e2
SantiagoC16 <6317@holbertonstudents.com>
2023-06-02 16:11:03 -0700

    task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def fizzbuzz():
+    for n in range(0, 101):
+        if n != 0 and n % 3 == 0 and n % 5 != 0:
+            print("Fizz".format(n), end=" ")
+        elif n != 0 and n % 5 == 0 and n % 3 != 0:
+            print("Buzz".format(n), end=" ")
+        elif n != 0 and n % 5 == 0 and n % 3 == 0:
+            print("FizzBuzz".format(n), end=" ")
+        else:
+            print("{:01d}".format(n), end=" ")

869db844dd866562d256f2e3e6d25267363d7658
SantiagoC16 <6317@holbertonstudents.com>
2023-06-02 13:25:41 -0700

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 the_str = "{:02d}"
 for n in range(0, 100):
     if n != 99:
-        print("{}, ".format(n ), end="")
+        print("{}, ".format(n), end="")
     else:
         print("{}".format(n))

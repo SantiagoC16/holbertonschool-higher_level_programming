307315601a7d348db0e193e131163cf61033abe6
SantiagoC16 <6317@holbertonstudents.com>
2023-06-02 12:33:20 -0700

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for n in range(0, 10):
     for m in range (1, 10):
         if n == 0 and m == 1:
             print("{}{}".format(n, m), end="")
-        elif m != n:
+        elif n != m and m > n:
             print(", {}{}".format(n, m), end="")
 print()

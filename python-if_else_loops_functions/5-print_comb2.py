be68e8c9897a7cba5df224c6d9f37ac83b2fe0af
SantiagoC16 <6317@holbertonstudents.com>
2023-06-02 13:27:45 -0700

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-the_str = "{:02d}"
 for n in range(0, 100):
     if n != 99:
-        print("{the_str}, ".format(n), end="")
+        print("{:02d}, ".format(n), end="")
     else:
-        print("{the_str}".format(n))
+        print("{:02d}".format(n))

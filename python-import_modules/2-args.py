e51fc75da8f02abbfe1ca0c7b7038db649c61a8a
SantiagoC16 <6317@holbertonstudents.com>
2023-06-03 10:25:49 -0700

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     arguments = len(sys.argv) - 1
     if arguments == 0:
-        print("{} arguments.".format(arguments))
+        print("0 arguments.")
     elif arguments == 1:
-        print("{} argument:".format(arguments))
+        print("1 argument:")
     else:
         print("{} arguments:".format(arguments))
     for argc in range(1, arguments + 1):

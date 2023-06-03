e27a64453fab190e660d770947d7a2e784f35f6a
SantiagoC16 <6317@holbertonstudents.com>
2023-06-03 11:35:09 -0700

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    suma = 0
+    for numbers in range(len(sys.argv) - 1):
+        suma = suma + int(sys.argv[numbers + 1])
+    print("{}".format(suma))

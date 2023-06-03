d4160aa756fc668e6b861d4d2d743902a2dc858f
SantiagoC16 <6317@holbertonstudents.com>
2023-06-03 10:23:15 -0700

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+if __name__ == "__main__":
+    import sys
+    arguments = len(sys.argv) -1
+    if arguments == 0:
+        print("{} arguments.".format(arguments))
+    elif arguments == 1:
+        print("{} argument:".format(arguments))
+    else:
+        print("{} arguments:".format(arguments))
+    for argc in range(1, arguments + 1):
+        print("{}: {}".format(argc, sys.argv[argc]))

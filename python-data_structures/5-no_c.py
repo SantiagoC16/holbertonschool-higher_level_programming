b80fa99de43f74cd15a1e2cdb46421f943f0d0d0
Santiago Caritat <6317@holbertonstudents.com>
2023-06-05 07:09:32 -0700

    task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
-    for l in my_string:
-        if l != "c" and l != "C":
-            new_str += l
+    for i in my_string:
+        if i != "c" and i != "C":
+            new_str += i
     return new_str

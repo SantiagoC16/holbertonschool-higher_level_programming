ede77e4a3692a2f4fe92aa3ddb0b7df88cd2a70b
Santiago Caritat <6317@holbertonstudents.com>
2023-06-05 08:49:21 -0700

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for n in range(len(matrix)):
         for m in range(len(matrix[n])):
-            if  m != len(matrix[m]) - 1:
+            if m != len(matrix[m]) - 1:
                 print("{:d} ".format(matrix[n][m]), end="")
             else:
                 print("{:d}".format(matrix[n][m]), end="")

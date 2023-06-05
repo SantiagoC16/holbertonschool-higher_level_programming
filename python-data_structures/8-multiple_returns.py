c9d0c90214f40caea705a61b708ae6c95e96392a
Santiago Caritat <6317@holbertonstudents.com>
2023-06-05 10:54:43 -0700

    task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     var = len(sentence)
-    last = sentence[0]
     if sentence:
+        last = sentence[0]
         return_tuple = (var, last)
         return return_tuple
     else:

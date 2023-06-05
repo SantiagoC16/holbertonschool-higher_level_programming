4685c1a6b9cc185ffa7958b91d0ef10c369f567f
Santiago Caritat <6317@holbertonstudents.com>
2023-06-05 10:13:57 -0700

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+    my_list = [0, 0]
+    for n in range(len(tuple_a)):
+        if n >= 2:
+            break
+        my_list[n] = tuple_a[n]
+    for m in range(len(tuple_b)):
+        if m >= 2:
+            break
+        my_list[m] = tuple_b[m]
+    tuple3 = (my_list[0], my_list[1])
+    return tuple3

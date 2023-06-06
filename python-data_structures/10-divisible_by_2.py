ff88cfd23f6f220e3afd30589aa11daeb19487a6
Santiago Caritat <6317@holbertonstudents.com>
2023-06-06 07:10:25 -0700

    task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    new_list = []
     for n in my_list:
         if n % 2 == 0:
-            return True
-        else:
-            return False
+            new_list = new_list.append(True)
+        if n % 2 != 0:
+            new_list = new_list.append(False)

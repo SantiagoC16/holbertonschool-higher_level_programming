60aec5f038a0f21104fbcff04f738e17e3f9f5d9
Santiago Caritat <6317@holbertonstudents.com>
2023-06-09 10:08:16 -0700

    task 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key:
+    if key in a_dictionary:
         a_dictionary.pop(key)
-    else: 
-        return a_dictionary
+    return a_dictionary

ae9d5ad158521dcfd9a3c09e318ee1c01cbb3508
Santiago Caritat <6317@holbertonstudents.com>
2023-06-09 06:13:58 -0700

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    
+    square = []
+    for n in range(len(matrix)):
+        square.append(list(map(lambda x: x ** 2, matrix[n])))
+    return square

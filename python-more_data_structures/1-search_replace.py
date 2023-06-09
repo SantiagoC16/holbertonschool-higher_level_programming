4f0ce41e1b4b568d69f40c88ec0372abd4de3bda
Santiago Caritat <6317@holbertonstudents.com>
2023-06-09 06:45:56 -0700

    task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    
+    new = my_list.copy()
+    for n in range(len(new)):
+        if new[n] == search:
+            new[n] = replace
+    return new

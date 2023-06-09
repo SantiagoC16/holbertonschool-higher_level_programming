6abdd087e4136e7c9a05054a9ad2db76422ba2f8
Santiago Caritat <6317@holbertonstudents.com>
2023-06-09 09:50:32 -0700

    task 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    dicti = a_dictionary.update()
-    return dicti
+    new = {key: value}
+    a_dictionary.update(new)
+    return a_dictionary

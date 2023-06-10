8ac660acbe45eee17d15779ff02596f8a91ce311
SantiagoC16 <6317@holbertonstudents.com>
2023-06-10 14:37:04 -0700

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        print("{Inside result: }".format(ab))
+        print("Inside result: {}".format(ab))
         return ab

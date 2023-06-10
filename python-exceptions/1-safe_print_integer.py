c18ba086fe9972b637076a71717d49fbe9c5d58b
SantiagoC16 <6317@holbertonstudents.com>
2023-06-10 14:05:47 -0700

    task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if isinstance(value, int):
-        try:
-            print("{}".format(value))
-            return True
-        except Exception:
-            return False
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        return False

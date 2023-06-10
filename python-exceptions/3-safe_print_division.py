9f05535d74807dd0f32065a5130dab3dc80a4c57
SantiagoC16 <6317@holbertonstudents.com>
2023-06-10 14:34:14 -0700

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    
+    try:
+        if a == 0:
+            return None
+    except ZeroDivisionError:
+        if b == 0:
+            return None
+    finally:
+        ab = a + b
+        print("{}".format(ab))
+        return ab

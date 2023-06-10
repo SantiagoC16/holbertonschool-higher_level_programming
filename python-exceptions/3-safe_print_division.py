d7449b7f8056792dbeefdd6140bfbff8b0469a30
SantiagoC16 <6317@holbertonstudents.com>
2023-06-10 14:39:20 -0700

    task 0

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         ab = a / b
     except Exception:
-        return None
+        ab = None
     finally:
         print("Inside result: {}".format(ab))
         return ab

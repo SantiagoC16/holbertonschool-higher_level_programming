76989f6d8f85515bd5886d64776a5049af4f18f5
SantiagoC16 <6317@holbertonstudents.com>
2023-06-10 10:58:35 -0700

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,4 +5,12 @@ def roman_to_int(roman_string):
     if roman_string is None:
         return 0
     else:
-        roman_number = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,"D": 500, "M": 1000}
+        roman_number = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }

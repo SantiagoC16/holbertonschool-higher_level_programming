111497378f96a86ca829780aaa16347deec9068b
SantiagoC16 <6317@holbertonstudents.com>
2023-06-10 13:56:20 -0700

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    
+    for p in range(x):
+        try:
+            print(my_list[p], end="")
+        except Exception:
+            print()
+            return p 
+    print()
+    return x

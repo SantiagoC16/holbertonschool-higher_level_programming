003390e6a7adf9cf8bf4e9f33fdb6ceaa5f5c005
SantiagoC16 <6317@holbertonstudents.com>
2023-06-10 14:19:18 -0700

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    
+    printed = 0
+    try:
+        for p in my_list:
+            if isinstance(p, int):
+                print("{}".format(p), end="")
+                printed += 1
+                if printed == x:
+                    break
+    except IndexError:
+        pass
+    print()
+    return printed

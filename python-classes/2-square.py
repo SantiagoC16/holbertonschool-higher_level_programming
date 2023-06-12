25292cbbf9815f9703ea5b68ce41b9636ce8ade5
Santiago Caritat <6317@holbertonstudents.com>
2023-06-12 09:41:45 -0700

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Write a class Square that defines a square """
+
+
+class Square:
+    """a class define square"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError
+        if size < 0:
+            raise ValueError
+        self.__size = size

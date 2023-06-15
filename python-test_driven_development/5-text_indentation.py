ca79e34b0da28a43691b3e485edc171ccf66e385
Santiago Caritat <6317@holbertonstudents.com>
2023-06-15 10:54:20 -0700

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def text_indentation(text):
-    
+    if not isinstance(text, str):
+        TypeError("text must be a string")
+    for t in text:
+        if t == ".":
+            a
+        if t == ":":
+            a
+        if t == "?":
+            a

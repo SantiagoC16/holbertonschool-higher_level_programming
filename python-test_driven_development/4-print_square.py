39d7b4f067b5c6ab25678cdc5662e3a0636827c3
Santiago Caritat <6317@holbertonstudents.com>
2023-06-15 10:42:56 -0700

    task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
+"""task 3"""
+
+
 def print_square(size):
-    
+    """_summary_
+
+    Args:
+        size (_type_): _description_
+
+    Raises:
+        TypeError: _description_
+        ValueError: _description_
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for row in range(size):
+        for col in range(size):
+            print("#", end="")
+        print()

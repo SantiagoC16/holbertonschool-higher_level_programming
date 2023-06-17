ddb5d5b0557288aa19d2e64f3a7fa387016ceab4
SantiagoC16 <6317@holbertonstudents.com>
2023-06-17 10:45:58 -0700

    task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -19,21 +19,20 @@ def matrix_divided(matrix, div):
         _type_: _description_
     """
 
-    matrix_copy = matrix.copy()
+    new_m = matrix.copy()
     error = "matrix must be a matrix (list of lists) of integers/floats"
-    if not isinstance(matrix_copy, list):
+    if not isinstance(new_m, list):
         raise TypeError(error)
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    lenght = len(matrix_copy[0])
-    for row in range(len(matrix_copy)):
-        if lenght != len(matrix_copy[row]):
+    lenght = len(new_m[0])
+    for row in range(len(new_m)):
+        if lenght != len(new_m[row]):
             raise TypeError("Each row of the matrix must have the same size")
-        for column in range(len(matrix_copy[row])):
-            if not isinstance(matrix_copy[row][column], (int, float)):
+        for column in range(len(new_m[row])):
+            if not isinstance(new_m[row][column], (int, float)):
                 raise TypeError(error)
-            matrix_copy[row] = list(
-                map(lambda x: round((x / div), 2), matrix[row]))
-    return matrix_copy
+            new_m[row] = list(map(lambda x: round((x / div), 2), matrix[row]))
+    return new_m

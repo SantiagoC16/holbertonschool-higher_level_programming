2e3828f72e95f399e3c3985aa54a52f96d7d1ea9
SantiagoC16 <6317@holbertonstudents.com>
2023-06-17 12:01:59 -0700

    task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -30,7 +30,7 @@ def matrix_divided(matrix, div):
     if not new_m:
         return new_m
     for row in range(len(new_m)):
-        if not isinstance(new_m[row], list):
+        if not matrix[row] or not isinstance(new_m[row], list):
             raise TypeError(error)
         lenght = len(new_m[0])
         if len(new_m[row]) != lenght:

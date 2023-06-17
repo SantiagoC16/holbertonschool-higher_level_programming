b10dfe8435d21b175fd77a06af8102a7edf9e7de
SantiagoC16 <6317@holbertonstudents.com>
2023-06-17 12:31:03 -0700

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,15 +4,23 @@
 
 def text_indentation(text):
     """task 5"""
-
     if not isinstance(text, str):
-        TypeError("text must be a string")
-    flag = False
-    for t in text:
-        if t == "." or t == ":" or t == "?":
-            print(t)
-            print()
-            flag = True
-        elif t != ' ' or not flag:
-            print(t, end="")
-            flag = False
+        raise TypeError("text must be a string")
+    bool = False
+    i = 0
+    while i < len(text):
+        if bool:
+            bool = False
+            continue
+        if text[i] == "." or text[i] == ":" or text[i] == "?":
+            print(f"{text[i]}")
+            print("")
+            j = i + 1
+            count = 0
+            while j < len(text) and text[j] == " ":
+                count += 1
+                j += 1
+            i += count
+        else:
+            print(f"{text[i]}", end="")
+        i += 1

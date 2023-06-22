a5a4fee620f810f273f67416bf610de960bf31b6
SantiagoC16 <6317@holbertonstudents.com>
2023-06-22 10:50:01 -0700

    task 6

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""task 5"""
+
+
+class BaseGeometry:
+    """comment"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not type(value) is int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

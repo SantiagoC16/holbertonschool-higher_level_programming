6ffd0fa70e84597eaeb34d2923c766b5088c9dbc
SantiagoC16 <6317@holbertonstudents.com>
2023-06-22 11:06:55 -0700

    task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""task 9"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """comment"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self._Rectangule__width__width = width
+        self._Rectangule__height = height
+
+    def area(self):
+        return self._Rectangle__height * self._Rectangle__width
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self._Rectangle__width,
+                                          self._Rectangle__height)

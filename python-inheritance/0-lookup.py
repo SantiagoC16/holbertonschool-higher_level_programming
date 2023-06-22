41c4f1db02b9530ca589df38c6023a72ca52b2bf
SantiagoC16 <6317@holbertonstudents.com>
2023-06-22 10:07:17 -0700

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,6 @@
 
 
 def lookup(obj):
-    """return the list of available attributes and aethods"""
+    """returns the list of available attributes and methods of an object"""
 
     return dir(obj)

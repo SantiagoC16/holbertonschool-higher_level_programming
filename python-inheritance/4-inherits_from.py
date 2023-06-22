576d76f96350c71ece11c31975e13d91e6a93958
SantiagoC16 <6317@holbertonstudents.com>
2023-06-22 10:27:45 -0700

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""task 4"""
+
+
 def inherits_from(obj, a_class):
-    
+    """comment"""
+    if type(obj) != a_class:
+        return isinstance(obj, a_class)
+    else:
+        return False

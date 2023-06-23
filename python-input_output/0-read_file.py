0227b4b8fa80f9dfddb6dce673ed5801a24e3398
Santiago Caritat <6317@holbertonstudents.com>
2023-06-23 07:47:26 -0700

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""task 0"""
+
+
 def read_file(filename=""):
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+    """
+
     with open(filename, mode="r", encoding="utf-8") as file:
         print(file.read(), end="")

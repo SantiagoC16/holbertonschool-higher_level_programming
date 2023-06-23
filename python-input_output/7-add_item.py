3892a983e8689125162870ae62b8d38d6ecc9b22
Santiago Caritat <6317@holbertonstudents.com>
2023-06-23 10:32:07 -0700

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,8 +7,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if os.path.exist("add_item.json"):
-    """comment"""
-
     list = load_from_json_file("add_item.json")
     for _read in range(1, len(argv)):
         list.append(argv[_read])

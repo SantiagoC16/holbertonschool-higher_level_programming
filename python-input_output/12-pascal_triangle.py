5fe4893e091dcef44923ffb7611778abee67b8ea
Santiago Caritat <6317@holbertonstudents.com>
2023-06-23 12:17:22 -0700

    task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+def pascal_triangle(n):
+    list = [[1]]
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [[1]]
+    else:
+        for one in range(n):
+            list.append([1])
+            for two in range(one):
+                if two == one - 1:
+                    list[one].append(1)
+                else:
+                    list[one].append(
+                        list[one - 1][two + 1] + list[one - 1][two])
+        return list

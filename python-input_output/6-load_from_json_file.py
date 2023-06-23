285bdc27318d52a3498e72ee8c27536f0da44070
Santiago Caritat <6317@holbertonstudents.com>
2023-06-23 09:07:12 -0700

    task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""task 6"""
+import json
+
+
 def load_from_json_file(filename):
-    
+    """_summary_
+
+    Args:
+        my_obj (_type_): _description_
+        filename (_type_): _description_
+    """
+
+    with open(filename, mode="r", encoding="utf-8") as file:
+        return json.loads(file.read())

9dba7e5ec3960f9cb6434b54bf48a39a23b488f3
Santiago Caritat <6317@holbertonstudents.com>
2023-06-23 07:54:11 -0700

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+"""task 0"""
+
+
 def write_file(filename="", text=""):
-    
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+
+    Returns:
+        _type_: _description_
+    """
+
+    with open(filename, mode="w", encoding="utf-8") as file:
+        file.write(text)
+        return len(text)

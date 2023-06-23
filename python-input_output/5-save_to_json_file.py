817862579d0d49c8f753e73aac5eee8776cf5013
Santiago Caritat <6317@holbertonstudents.com>
2023-06-23 08:55:57 -0700

    task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -12,4 +12,4 @@ def save_to_json_file(my_obj, filename):
     """
 
     with open(filename, mode="w", encoding="utf-8") as file:
-        file.write(json.dump(my_obj, file))
+        json.dump(my_obj, file)

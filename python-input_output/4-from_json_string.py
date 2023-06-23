5603526c9d88ba82f0fc68fcdef019fad9a9b36f
Santiago Caritat <6317@holbertonstudents.com>
2023-06-23 08:18:16 -0700

    task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""task 2"""
+import json
+
+
 def from_json_string(my_str):
-    
+    """_summary_
+
+    Args:
+        my_obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    return json.load(my_str)

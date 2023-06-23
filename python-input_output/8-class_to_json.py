414334eaa0bbbcc4a86ad7577b0a99853c36bfbb
Santiago Caritat <6317@holbertonstudents.com>
2023-06-23 11:06:01 -0700

    task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""task 8"""
+
+
+def class_to_json(obj):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    return obj.__dict__

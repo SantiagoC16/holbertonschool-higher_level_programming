8351cf3cee03720c5175fe620830edb0a8b67e96
Santiago Caritat <6317@holbertonstudents.com>
2023-06-23 08:00:02 -0700

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+"""task 0"""
+
+
 def append_write(filename="", text=""):
-    
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+
+    Returns:
+        _type_: _description_
+    """
+
+    with open(filename, mode="a", encoding="utf-8") as file:
+        file.write(text)
+        return len(text)

0b4eb8d93691c30ff158cb2dbae466e4a9e84993
Santiago Caritat <6317@holbertonstudents.com>
2023-06-23 10:31:26 -0700

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+from sys import argv
+import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""task 7"""
+
+
+if os.path.exist("add_item.json"):
+    """comment"""
+
+    list = load_from_json_file("add_item.json")
+    for _read in range(1, len(argv)):
+        list.append(argv[_read])
+        save_to_json_file("add.item.json")
+else:
+    list = []
+    for _read in range(1, len(argv)):
+        list.append(argv[_read])
+        save_to_json_file("add.item.json")

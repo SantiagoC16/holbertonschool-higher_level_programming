0fbf253e6898a1e2e433f25499a3f8d067a66211
Santiago Caritat <6317@holbertonstudents.com>
2023-06-27 10:31:07 -0700

    task 9

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -102,18 +102,29 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """public method update"""
 
-        for arg in args:
-            if args:
-                if len(args) > 0:
-                    self.id = args[0]
-                if len(args) > 1:
-                    self.__width = args[1]
-                if len(args) > 2:
-                    self.__height = args[2]
-                if len(args) > 3:
-                    self.__x = args[3]
-                if len(args) > 4:
-                    self.__y = args[4]
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                if k == "width":
+                    self.__width = v
+                if k == "height":
+                    self.__height = v
+                if k == "x":
+                    self.__x = v
+                if k == "y":
+                    self.__y = v

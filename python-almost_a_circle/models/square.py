c31cdc46f899495426996259aca2db2c581b153c
Santiago Caritat <6317@holbertonstudents.com>
2023-06-27 10:52:25 -0700

    task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -16,3 +16,12 @@ class Square(Rectangle):
 
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.size
+    
+    @size.setter
+    def size(self, value):
+        
+

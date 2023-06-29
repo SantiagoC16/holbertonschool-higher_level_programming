ae043bb78b4a8c4f29dc0b0e75e2ac2d1a6981ae
Santiago Caritat <6317@holbertonstudents.com>
2023-06-29 08:07:47 -0700

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base"""
+import json
 
 
 class Base:
@@ -13,3 +14,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """static method"""
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dump(list_dictionaries)
